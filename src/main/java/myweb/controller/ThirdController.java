8b04c53cf1befccf0c719cd662693ef4751cee67
Jeong Woo <netsong7@naver.com>
2017-01-10 14:47:16 +0900

    test

--- src/main/java/myweb/controller/ThirdController.java
+++ src/main/java/myweb/controller/ThirdController.java
@@ -3,11 +3,14 @@ package myweb.controller;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.AbstractController;
 
-public class ThirdController extends AbstractController{
-	@Override
+@Controller("third")
+public class ThirdController {
+	@RequestMapping("/third.spr1")
 	protected ModelAndView handleRequestInternal(HttpServletRequest arg0, HttpServletResponse arg1) throws Exception {
 		ModelAndView mv = 
 			new ModelAndView("/WEB-INF/views/result.jsp");
